7ea1f7ccc2c66a16d262df82438af39b0cde6a24
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-06-07 12:06:58 +0000

    moved the tariff repository to the repository package

--- src/main/java/com/gigacapstone/billingservice/repository/TariffRepository.java
+++ src/main/java/com/gigacapstone/billingservice/repository/TariffRepository.java
@@ -1,4 +1,4 @@
-package com.gigacapstone.billingservice;
+package com.gigacapstone.billingservice.repository;
 
 import com.gigacapstone.billingservice.model.TariffPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
