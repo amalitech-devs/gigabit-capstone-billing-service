8924a41f90231e110ad0447b6041cae02dfd2c80
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-06-08 16:36:57 +0000

    added bundle controller method

--- src/main/java/com/gigacapstone/billingservice/dto/VoicePackageDTO.java
+++ src/main/java/com/gigacapstone/billingservice/dto/VoicePackageDTO.java
@@ -1,4 +1,16 @@
 package com.gigacapstone.billingservice.dto;
 
-public record VoicePackageDTO(String name, double price) {
+import com.gigacapstone.billingservice.enums.ExpirationRate;
+import com.gigacapstone.billingservice.model.CallTime;
+import lombok.Data;
+
+@Data
+public class VoicePackageDTO{
+    String name;
+    double price;
+    Boolean isEnabled;
+    Boolean isVatApplied;
+    int vatPercentage;
+    ExpirationRate expirationRate;
+    CallTime callTime;
 }
