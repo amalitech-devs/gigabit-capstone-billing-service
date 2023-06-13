2cc02f7daf9a71a913b4fd4a1b8c97e53870bb07
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-06-13 10:18:08 +0000

    removed mapper utils component

--- src/main/java/com/gigacapstone/billingservice/service/TariffServiceImpl.java
+++ src/main/java/com/gigacapstone/billingservice/service/TariffServiceImpl.java
@@ -1,26 +1,24 @@
 package com.gigacapstone.billingservice.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gigacapstone.billingservice.dto.BundlePackageDTO;
 import com.gigacapstone.billingservice.dto.VoicePackageDTO;
 import com.gigacapstone.billingservice.exception.EntityAlreadyExistException;
 import com.gigacapstone.billingservice.model.BundlePackage;
 import com.gigacapstone.billingservice.model.VoicePackage;
 import com.gigacapstone.billingservice.repository.TariffRepository;
-import com.gigacapstone.billingservice.utils.Mapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class TariffServiceImpl implements TariffService {
 
     private final TariffRepository tariffRepository;
-    private final Mapper objectMapper;
+    private final ObjectMapper mapper;
 
     @Override
     public VoicePackageDTO createVoicePackage(VoicePackageDTO voicePackage) {
@@ -32,7 +30,7 @@ public class TariffServiceImpl implements TariffService {
             throw new EntityAlreadyExistException("Package Name already exists");
         }
         voicePackage.setIsEnabled(false);
-        tariffRepository.save(objectMapper.mapVoicePackageDTOToVoicePackage(voicePackage));
+        tariffRepository.save(mapper.convertValue(voicePackage, VoicePackage.class));
 
         return voicePackage;
     }
@@ -47,7 +45,7 @@ public class TariffServiceImpl implements TariffService {
             throw new EntityAlreadyExistException("Package Name already exists");
         }
         bundlePackage.setIsEnabled(false);
-        tariffRepository.save(objectMapper.mapBundlePackageDTOToBundlePackage(bundlePackage));
+        tariffRepository.save(mapper.convertValue(bundlePackage, BundlePackage.class));
 
         return bundlePackage;
     }
@@ -56,14 +54,14 @@ public class TariffServiceImpl implements TariffService {
     public Page<BundlePackageDTO> listAllBundlePackages(int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         Page<BundlePackage> bundlePackages = tariffRepository.findBundlePackages(pageable);
-        return bundlePackages.map(objectMapper::mapBundlePackageToBundlePackageDTO);
+        return bundlePackages.map(bundle -> mapper.convertValue(bundle, BundlePackageDTO.class));
     }
 
     @Override
     public Page<VoicePackageDTO> listAllVoicePackages(int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         Page<VoicePackage> voicePackages = tariffRepository.findVoicePackages(pageable);
-        return voicePackages.map(objectMapper::mapVoicePackageToVoicePackageDTO);
+        return voicePackages.map(voice -> mapper.convertValue(voice, VoicePackageDTO.class));
     }
 
     private boolean doesPackageAlreadyExist(String packageName) {
