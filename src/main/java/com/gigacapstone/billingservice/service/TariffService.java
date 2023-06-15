4c5d90108db726c99c7a89fa06634470734a48cf
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-06-15 08:33:24 +0000

    added methods for getting all packages

--- src/main/java/com/gigacapstone/billingservice/service/TariffService.java
+++ src/main/java/com/gigacapstone/billingservice/service/TariffService.java
@@ -1,9 +1,12 @@
 package com.gigacapstone.billingservice.service;
 
+import com.gigacapstone.billingservice.dto.AllPackagesDTO;
 import com.gigacapstone.billingservice.dto.BundlePackageDTO;
 import com.gigacapstone.billingservice.dto.VoicePackageDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface TariffService {
 
@@ -14,4 +17,6 @@ public interface TariffService {
     Page<BundlePackageDTO> listAllBundlePackages(Pageable pageable);
 
     Page<VoicePackageDTO> listAllVoicePackages(Pageable pageable);
+
+    AllPackagesDTO listAllPackages(Pageable pageable);
 }
