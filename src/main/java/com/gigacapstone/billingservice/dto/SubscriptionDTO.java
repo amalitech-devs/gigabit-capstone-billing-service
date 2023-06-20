e775ccf6dead542ebee9d2ab593d88dfc58a2ec0
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-06-20 12:14:44 +0000

    refactor the subscription dto to hold the tariff type

--- src/main/java/com/gigacapstone/billingservice/dto/SubscriptionDTO.java
+++ src/main/java/com/gigacapstone/billingservice/dto/SubscriptionDTO.java
@@ -1,5 +1,6 @@
 package com.gigacapstone.billingservice.dto;
 
+import com.gigacapstone.billingservice.enums.TariffType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,4 +16,5 @@ public class SubscriptionDTO {
     private LocalDate expiryDate;
     private String status;
     private UUID userId;
+    private TariffType type;
 }
