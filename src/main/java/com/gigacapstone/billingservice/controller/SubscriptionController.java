e0477c4ccfc9d3379f08c452f6684f4afc1d7402
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-06-29 09:09:31 +0000

    fixed issue on instance

--- src/main/java/com/gigacapstone/billingservice/controller/SubscriptionController.java
+++ src/main/java/com/gigacapstone/billingservice/controller/SubscriptionController.java
@@ -4,11 +4,11 @@ import com.gigacapstone.billingservice.dto.SubscriptionDTO;
 import com.gigacapstone.billingservice.service.SubscriptionService;
 import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpStatus;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -19,7 +19,7 @@ public class SubscriptionController {
     private final SubscriptionService subscriptionService;
 
     @GetMapping("/{userId}")
-    ResponseEntity<List<SubscriptionDTO>> getAllSubscriptionForUser(@PathVariable UUID userId, Pageable pageable){
+    ResponseEntity<Page<SubscriptionDTO>> getAllSubscriptionForUser(@PathVariable UUID userId, Pageable pageable){
         return ResponseEntity
                 .ok(subscriptionService.getAllSubscriptionsOfUser(userId, pageable));
     }
