00305d4a21a40bce8030532846f0148ec21d2092
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-06-29 10:20:04 +0000

    added search functionality to the subscription

--- src/main/java/com/gigacapstone/billingservice/repository/SubscriptionRepository.java
+++ src/main/java/com/gigacapstone/billingservice/repository/SubscriptionRepository.java
@@ -4,12 +4,14 @@ import com.gigacapstone.billingservice.model.Subscription;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
-public interface SubscriptionRepository extends JpaRepository<Subscription, UUID>,
-        PagingAndSortingRepository<Subscription, UUID> {
+public interface SubscriptionRepository extends JpaRepository<Subscription, UUID>{
 
     Page<Subscription> findAllByUserId(UUID userId, Pageable pageable);
+
+    @Query("SELECT s FROM Subscription s WHERE s.userId = :id AND s.tariffName = :tariffName")
+    Page<Subscription> searchForUserSubscription(UUID id, String tariffName, Pageable pageable);
 }
