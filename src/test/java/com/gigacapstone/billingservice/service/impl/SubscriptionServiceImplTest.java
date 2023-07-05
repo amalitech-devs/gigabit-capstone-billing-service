e24c565ced93d5abaf80a0294de08a2f39b9ce8a
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-05 15:09:30 +0000

    wrote unit test for subscription service

--- src/test/java/com/gigacapstone/billingservice/service/impl/SubscriptionServiceImplTest.java
+++ src/test/java/com/gigacapstone/billingservice/service/impl/SubscriptionServiceImplTest.java
@@ -13,9 +13,11 @@ import com.gigacapstone.billingservice.model.TariffPlan;
 import com.gigacapstone.billingservice.repository.BundlePackageRepository;
 import com.gigacapstone.billingservice.repository.SubscriptionRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -28,181 +30,213 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 class SubscriptionServiceImplTest {
+
+    @InjectMocks
+    private SubscriptionServiceImpl subscriptionService;
+
+    @Mock
+    private ObjectMapper mapper;
 
     @Mock
     private SubscriptionRepository subscriptionRepository;
 
     @Mock
     private BundlePackageRepository bundlePackageRepository;
-
-
-    @Mock
-    private ObjectMapper mapper;
-
-    @InjectMocks
-    private SubscriptionServiceImpl subscriptionService;
+    private BundlePackage bundlePackage;
+    private SubscriptionDTO subscriptionDTO;
+    private Subscription subscription;
 
     @BeforeEach
-    public void setUp() {
+    void setUp(){
         MockitoAnnotations.openMocks(this);
-    }
 
-    @Test
-    void testCreateSubscription() {
-        // Mock input data
-        SubscriptionDTO subscriptionDTO = new SubscriptionDTO();
-        subscriptionDTO.setUserId(UUID.randomUUID());
-        subscriptionDTO.setTariffName("Tariff 1");
-        subscriptionDTO.setType(TariffType.BUNDLE);
-
-        // Mock TariffPlan and its related data
-        TariffPlan tariffPlan = new BundlePackage();
+        TariffPlan tariffPlan = new TariffPlan();
+        tariffPlan.setId(5);
+        tariffPlan.setName("Sika Kokoo");
         tariffPlan.setExpirationRate(ExpirationRate.ONE_MONTH);
 
-        // Mock converted Subscription object
-        Subscription subscription = new Subscription();
-        subscription.setTariffName(tariffPlan.getName());
+        bundlePackage = new BundlePackage();
+        bundlePackage.setName("Sika Kokoo");
+        bundlePackage.setExpirationRate(ExpirationRate.ONE_MONTH);
+        bundlePackage.setPrice(2.00);
+
+        subscriptionDTO = new SubscriptionDTO();
+        subscriptionDTO.setTariffName("Sika Kokoo");
+        subscriptionDTO.setType(TariffType.BUNDLE);
+        subscriptionDTO.setExpiryDate(LocalDate.now().plusMonths(1));
+        subscriptionDTO.setBillingType(BillingType.AUTO_RENEWAL);
+        subscriptionDTO.setPrice(2.00);
+
+
+        subscription = new Subscription();
+        subscription.setTariffName("Sika Kokoo");
+        subscription.setType(TariffType.BUNDLE);
         subscription.setExpiryDate(LocalDate.now().plusMonths(1));
-        subscription.setStatus("current");
+        subscription.setBillingType(BillingType.AUTO_RENEWAL);
 
-        // Mock saved Subscription object
-        Subscription savedSubscription = new Subscription();
-        savedSubscription.setId(UUID.randomUUID());
+    }
 
-        // Mock ObjectMapper conversion
-        SubscriptionDTO savedSubscriptionDTO = new SubscriptionDTO();
-        savedSubscriptionDTO.setId(savedSubscription.getId());
+    @Test
+    @DisplayName("Create_Bundle_Subscription_Success")
+    void testCreateBundleSubscription_pass_successfully(){
 
-        // Mock repository methods
-        when(bundlePackageRepository.findBundlePackageByName("Tariff 1")).thenReturn(Optional.of((BundlePackage) tariffPlan));
-        when(subscriptionRepository.save(subscription)).thenReturn(savedSubscription);
-        when(mapper.convertValue(savedSubscription, SubscriptionDTO.class)).thenReturn(savedSubscriptionDTO);
+        Mockito.when(bundlePackageRepository.findBundlePackageByName(any())).thenReturn(Optional.of(bundlePackage));
+        Mockito.when(mapper.convertValue(any(), eq(Subscription.class))).thenReturn(subscription);
+        Mockito.when(subscriptionRepository.save(any())).thenReturn(subscription);
+        Mockito.when(mapper.convertValue(any(), eq(SubscriptionDTO.class))).thenReturn(subscriptionDTO);
 
-        // Call the service method
-        SubscriptionDTO resultDTO = subscriptionService.createSubscription(subscriptionDTO);
+        //Act
+        SubscriptionDTO result = subscriptionService.createSubscription(subscriptionDTO);
 
-        // Verify the repository method calls
-        verify(bundlePackageRepository, times(1)).findBundlePackageByName("Tariff 1");
+        assertNotNull(result);
+        assertEquals(subscriptionDTO.getType(), result.getType());
+        assertEquals(subscriptionDTO.getTariffName(), result.getTariffName());
+
         verify(subscriptionRepository, times(1)).save(subscription);
-        verify(mapper, times(1)).convertValue(savedSubscription, SubscriptionDTO.class);
-
-        // Verify the result
-        assertNotNull(resultDTO);
-        assertEquals(savedSubscription.getId(), resultDTO.getId());
     }
 
     @Test
-    void testGetAllSubscriptionsOfUser() {
-        // Mock input data
+    @DisplayName("Get_All_Subscriptions_of_User")
+    void testGetAllSubscriptionsOfUser(){
         UUID userId = UUID.randomUUID();
-        Pageable pageable = Pageable.ofSize(10).withPage(0);
+        Pageable pageable = Pageable.unpaged();
 
-        // Mock Subscription objects
+        //Subscription List
         List<Subscription> subscriptionList = new ArrayList<>();
-        subscriptionList.add(createSubscription(UUID.randomUUID(), userId, "Subscription 1"));
-        subscriptionList.add(createSubscription(UUID.randomUUID(), userId, "Subscription 2"));
+        subscriptionList.add(subscription);
 
-        // Mock Page object
-        Page<Subscription> subscriptionPage = new PageImpl<>(subscriptionList, pageable, subscriptionList.size());
+        //Mock the subscriptionRepository to return the subscriptionList
+        Page<Subscription> subscriptions = new PageImpl<>(subscriptionList);
+        when(subscriptionRepository.findSubscriptionsByUserId(userId, pageable)).thenReturn(subscriptions);
 
-        // Mock ObjectMapper conversion
-        List<SubscriptionDTO> subscriptionDTOList = new ArrayList<>();
-        for (Subscription subscription : subscriptionList) {
-            SubscriptionDTO subscriptionDTO = new SubscriptionDTO();
-            subscriptionDTO.setId(subscription.getId());
-            subscriptionDTOList.add(subscriptionDTO);
-        }
-        Page<SubscriptionDTO> subscriptionDTOPage = new PageImpl<>(subscriptionDTOList, pageable, subscriptionDTOList.size());
+        when(mapper.convertValue(any(), eq(SubscriptionDTO.class))).thenReturn(subscriptionDTO);
+        when(bundlePackageRepository.findBundlePackageByName(any())).thenReturn(Optional.of(bundlePackage));
 
-        // Mock repository methods
-        when(subscriptionRepository.findSubscriptionsByUserId(userId, pageable)).thenReturn(subscriptionPage);
-        when(mapper.convertValue(any(Subscription.class), eq(SubscriptionDTO.class))).thenReturn(new SubscriptionDTO());
+        //Act
+        Page<SubscriptionDTO> result = subscriptionService.getAllSubscriptionsOfUser(userId, pageable);
 
-        // Call the service method
-        Page<SubscriptionDTO> resultPage = subscriptionService.getAllSubscriptionsOfUser(userId, pageable);
+        assertNotNull(result);
+        assertEquals(subscriptionDTO, result.getContent().get(0));
 
-        // Verify the repository method calls
         verify(subscriptionRepository, times(1)).findSubscriptionsByUserId(userId, pageable);
-        verify(mapper, times(subscriptionList.size())).convertValue(any(Subscription.class), eq(SubscriptionDTO.class));
-
-        // Verify the result
-        assertNotNull(resultPage);
-        assertEquals(subscriptionDTOPage.getTotalElements(), resultPage.getTotalElements());
-        assertEquals(subscriptionDTOPage.getContent().size(), resultPage.getContent().size());
+        verify(mapper, times(1)).convertValue(subscription, SubscriptionDTO.class);
     }
 
     @Test
-    void testCancelSubscription() {
-        // Mock input data
+    @DisplayName("Cancel_Subscription_Success")
+    void testCancelSubscription(){
         UUID subscriptionId = UUID.randomUUID();
 
-        // Mock Subscription object
+        // Create a mock Subscription object
         Subscription subscription = new Subscription();
         subscription.setId(subscriptionId);
         subscription.setBillingType(BillingType.AUTO_RENEWAL);
         subscription.setExpiryDate(LocalDate.now().plusDays(1));
 
-        // Mock repository methods
         when(subscriptionRepository.findById(subscriptionId)).thenReturn(Optional.of(subscription));
-        when(subscriptionRepository.save(subscription)).thenReturn(subscription);
 
-        // Call the service method
-        subscriptionService.cancelSubscription(subscriptionId);
+        assertDoesNotThrow(() -> subscriptionService.cancelSubscription(subscriptionId));
 
-        // Verify the repository method calls
+        // Verify the interactions and assertions
         verify(subscriptionRepository, times(1)).findById(subscriptionId);
         verify(subscriptionRepository, times(1)).save(subscription);
-
-        // Verify the subscription status
         assertEquals("cancelled", subscription.getStatus());
     }
 
     @Test
-    void testCancelSubscription_NotFoundException() {
-        // Mock input data
+    @DisplayName("Cancel_Subscription_Not_AutoRenewal")
+    void testCancelSubscription_NotAutoRenewal(){
         UUID subscriptionId = UUID.randomUUID();
 
-        // Mock repository methods
-        when(subscriptionRepository.findById(subscriptionId)).thenReturn(Optional.empty());
+        // Create a mock Subscription object
+        Subscription subscription = new Subscription();
+        subscription.setId(subscriptionId);
+        subscription.setBillingType(BillingType.ONE_TIME);
+        subscription.setExpiryDate(LocalDate.now().plusDays(1));
 
-        // Call the service method and verify the NotFoundException
-        assertThrows(NotFoundException.class, () -> subscriptionService.cancelSubscription(subscriptionId));
+        when(subscriptionRepository.findById(subscriptionId)).thenReturn(Optional.of(subscription));
 
-        // Verify the repository method calls
+        assertThrows(OperationFailedException.class,() -> subscriptionService.cancelSubscription(subscriptionId));
+
+        // Verify the interactions and assertions
         verify(subscriptionRepository, times(1)).findById(subscriptionId);
-        verify(subscriptionRepository, never()).save(any(Subscription.class));
+        verify(subscriptionRepository, never()).save(any());
     }
 
     @Test
-    void testCancelSubscription_OperationFailedException() {
-        // Mock input data
+    @DisplayName("Cancel_Subscription_Past_Expiry_Date")
+    void testCancelSubscription_PastExpiryDate(){
         UUID subscriptionId = UUID.randomUUID();
 
-        // Mock Subscription object
+        // Create a mock Subscription object
         Subscription subscription = new Subscription();
         subscription.setId(subscriptionId);
         subscription.setBillingType(BillingType.AUTO_RENEWAL);
-        subscription.setExpiryDate(LocalDate.now().plusDays(1));
+        subscription.setExpiryDate(LocalDate.now().minusDays(1));
 
-        // Mock repository methods
         when(subscriptionRepository.findById(subscriptionId)).thenReturn(Optional.of(subscription));
 
-        // Call the service method and verify the OperationFailedException
-        assertThrows(OperationFailedException.class, () -> subscriptionService.cancelSubscription(subscriptionId));
+        assertThrows(OperationFailedException.class,() -> subscriptionService.cancelSubscription(subscriptionId));
 
-        // Verify the repository method calls
+        // Verify the interactions and assertions
         verify(subscriptionRepository, times(1)).findById(subscriptionId);
-        verify(subscriptionRepository, never()).save(any(Subscription.class));
+        verify(subscriptionRepository, never()).save(any());
     }
 
-    private Subscription createSubscription(UUID id, UUID userId, String tariffName) {
-        Subscription subscription = new Subscription();
-        subscription.setId(id);
-        subscription.setUserId(userId);
-        subscription.setTariffName(tariffName);
-        return subscription;
+    @Test
+    @DisplayName("Cancel_Subscription_Subscription_Not_Found")
+    void testCancelSubscription_SubscriptionNotFound(){
+        UUID subscriptionId = UUID.randomUUID();
+
+        when(subscriptionRepository.findById(subscriptionId)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class,() -> subscriptionService.cancelSubscription(subscriptionId));
+
+        // Verify the interactions and assertions
+        verify(subscriptionRepository, times(1)).findById(subscriptionId);
+        verify(subscriptionRepository, never()).save(any());
+    }
+
+    @Test
+    @DisplayName("Search_for_User_Subs_By_TariffName")
+    void testSearchSubscriptionOfUserByTariffName(){
+        UUID userId = UUID.randomUUID();
+        String tariffName = "Sika Kokoo";
+        Pageable pageable = Pageable.unpaged();
+
+        List<Subscription> listOfSubscriptions = new ArrayList<>();
+        listOfSubscriptions.add(subscription);
+
+        Page<Subscription> subscriptions = new PageImpl<>(listOfSubscriptions);
+        when(subscriptionRepository.searchForUserSubscription(userId, tariffName.toLowerCase(), pageable)).thenReturn(subscriptions);
+
+        when(bundlePackageRepository.findBundlePackageByName(any())).thenReturn(Optional.of(bundlePackage));
+        when(mapper.convertValue(any(), eq(SubscriptionDTO.class))).thenReturn(subscriptionDTO);
+
+        Page<SubscriptionDTO> result = subscriptionService.searchSubscriptionsByName(userId, tariffName, pageable);
+
+        assertNotNull(result);
+        assertEquals(subscriptions.getSize(), result.getContent().size());
+
+        verify(subscriptionRepository, times(1)).searchForUserSubscription(userId, tariffName.toLowerCase(),pageable);
+    }
+
+    @Test
+    @DisplayName("Delete_Subscription_By_Id")
+    void testDeleteSubscriptionById(){
+        UUID subscriptionId = UUID.randomUUID();
+
+        subscription.setId(subscriptionId);
+
+        when(subscriptionRepository.findById(subscriptionId)).thenReturn(Optional.of(subscription));
+
+        subscriptionService.deleteSubscription(subscriptionId);
+
+        verify(subscriptionRepository, times(1)).deleteById(subscriptionId);
     }
 }
