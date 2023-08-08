221554261bdbdafffe6b86bbef5f5daab1b306ce
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-08-08 15:48:28 +0000

    controller integration test for tariff plan controller

--- src/test/java/com/gigacapstone/billingservice/TariffPlanControllerIntegrationTest.java
+++ src/test/java/com/gigacapstone/billingservice/TariffPlanControllerIntegrationTest.java
@@ -1,14 +1,26 @@
 package com.gigacapstone.billingservice;
 
-import com.gigacapstone.billingservice.controller.TariffPlanController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gigacapstone.billingservice.dto.VoicePackageDTO;
+import com.gigacapstone.billingservice.enums.ExpirationRate;
+import com.gigacapstone.billingservice.enums.TimeUnit;
+import com.gigacapstone.billingservice.model.CallTime;
+import jakarta.ws.rs.core.MediaType;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@WebMvcTest(TariffPlanController.class)
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
 @AutoConfigureMockMvc
-public class TariffPlanControllerIntegrationTest {
+@ActiveProfiles("test")
+class TariffPlanControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -18,5 +30,32 @@ public class TariffPlanControllerIntegrationTest {
             .concat(":")
             .concat(String.valueOf(port))
             .concat("/api/v1/tariff");
+
+    @Test
+    void createVoicePackage() throws Exception {
+        VoicePackageDTO voicePackageDTO = new VoicePackageDTO();
+        voicePackageDTO.setName("Test Name");
+        voicePackageDTO.setPrice(2.0);
+        voicePackageDTO.setIsEnabled(false);
+        voicePackageDTO.setIsVatApplied(true);
+        voicePackageDTO.setVatPercentage(20);
+        voicePackageDTO.setExpirationRate(ExpirationRate.TWO_WEEKS);
+        voicePackageDTO.setCallTime(new CallTime(TimeUnit.MINUTES, 20));
+
+        mockMvc.perform(MockMvcRequestBuilders.post(baseUrl.concat("/voice"))
+                .content(asJsonString(voicePackageDTO))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").exists());
+    }
+
+    private static String asJsonString(final Object object) {
+        try{
+            return new ObjectMapper().writeValueAsString(object);
+        }catch(Exception ex){
+            throw new RuntimeException(ex.getMessage());
+        }
+    }
 
 }
