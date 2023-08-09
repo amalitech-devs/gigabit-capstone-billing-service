07c5c0cfab7ba75fd7666a5f8d68638d1b103ba1
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-08-09 14:42:54 +0000

    controller integration test

--- src/test/java/com/gigacapstone/billingservice/TariffPlanControllerIntegrationTest.java
+++ src/test/java/com/gigacapstone/billingservice/TariffPlanControllerIntegrationTest.java
@@ -5,9 +5,13 @@ import com.gigacapstone.billingservice.dto.BundlePackageDTO;
 import com.gigacapstone.billingservice.dto.VoicePackageDTO;
 import com.gigacapstone.billingservice.enums.ExpirationRate;
 import com.gigacapstone.billingservice.enums.TimeUnit;
+import com.gigacapstone.billingservice.model.BundlePackage;
 import com.gigacapstone.billingservice.model.CallTime;
+import com.gigacapstone.billingservice.model.VoicePackage;
+import com.gigacapstone.billingservice.repository.BundlePackageRepository;
+import com.gigacapstone.billingservice.repository.VoicePackageRepository;
 import jakarta.ws.rs.core.MediaType;
-import net.bytebuddy.asm.Advice;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -18,6 +22,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -28,12 +35,56 @@ class TariffPlanControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private VoicePackageRepository voicePackageRepository;
+
+    @Autowired
+    private BundlePackageRepository bundlePackageRepository;
+
     @LocalServerPort
     private static int port;
     private static final String baseUrl = "http://localhost"
             .concat(":")
             .concat(String.valueOf(port))
             .concat("/api/v1/tariff");
+
+
+    @BeforeEach
+    void setup(){
+        VoicePackage voicePackage1 = new VoicePackage();
+        voicePackage1.setName("Test Name");
+        voicePackage1.setPrice(2.0);
+        voicePackage1.setIsEnabled(false);
+        voicePackage1.setIsVatApplied(true);
+        voicePackage1.setVatPercentage(20);
+        voicePackage1.setExpirationRate(ExpirationRate.TWO_WEEKS);
+        voicePackage1.setCallTime(new CallTime(TimeUnit.MINUTES, 20));
+
+        VoicePackage voicePackage2 = new VoicePackage();
+        voicePackage2.setName("Test Name 2");
+        voicePackage2.setPrice(2.0);
+        voicePackage2.setIsEnabled(false);
+        voicePackage2.setIsVatApplied(true);
+        voicePackage2.setVatPercentage(20);
+        voicePackage2.setExpirationRate(ExpirationRate.TWO_WEEKS);
+        voicePackage2.setCallTime(new CallTime(TimeUnit.MINUTES, 20));
+
+        voicePackageRepository.saveAll(List.of(voicePackage1, voicePackage2));
+
+        //Bundle Setup
+        BundlePackage bundlePackage = new BundlePackage();
+        bundlePackage.setName("Test Bundle");
+        bundlePackage.setPrice(12.0);
+        bundlePackage.setCallTime(new CallTime(TimeUnit.HOURS,2));
+        bundlePackage.setVatPercentage(15);
+        bundlePackage.setIsVatApplied(true);
+        bundlePackage.setExpirationRate(ExpirationRate.ONE_WEEK);
+        bundlePackage.setDataSize(100);
+        bundlePackage.setDownloadSpeed(12.0);
+        bundlePackage.setUploadSpeed(10.0);
+
+        bundlePackageRepository.save(bundlePackage);
+    }
 
     @Test
     void createVoicePackage() throws Exception {
@@ -80,7 +131,8 @@ class TariffPlanControllerIntegrationTest {
         mockMvc.perform(MockMvcRequestBuilders.get(baseUrl.concat("/voice"))
                 .accept(org.springframework.http.MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").exists());
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[*].name").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content", hasSize(2)));
     }
 
     private static String asJsonString(final Object object) {
