c35cebd6b684a9a3284972f2488c9b4c8e21567b
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-08-09 15:06:47 +0000

    controller integration testing

--- src/test/java/com/gigacapstone/billingservice/TariffPlanControllerIntegrationTest.java
+++ src/test/java/com/gigacapstone/billingservice/TariffPlanControllerIntegrationTest.java
@@ -25,6 +25,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -154,6 +155,18 @@ class TariffPlanControllerIntegrationTest {
                 .andExpect(jsonPath("$.content[*].name").exists())
                 .andExpect(jsonPath("$.content", hasSize(1)))
                 .andExpect(jsonPath("$.content[0].name").value("Test Bundle"));
+    }
+
+    @Test
+    void searchVoicePackages() throws Exception{
+        String requestParam = "name";
+        String paramValue = "Test";
+        mockMvc.perform(MockMvcRequestBuilders.get(baseUrl.concat("/voice/search"))
+                .accept(org.springframework.http.MediaType.APPLICATION_JSON)
+                .param(requestParam, paramValue))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", hasSize(2)));
     }
 
     private static String asJsonString(final Object object) {
